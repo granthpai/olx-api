309cedf0a2776218a6e73ebfdd7dd4358e49e6e2
Granth <granthpai1503@gmail.com>
2026-09-04 13:01:44 +0530

    add server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte(`{"status": "healthy"}`))
+	})
+
+	srv := http.Server{
+		Addr:   ":8080",
+		Handler: mux,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout: 60 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal("server failed: %v", err)
+	}
 }

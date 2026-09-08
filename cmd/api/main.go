06e21493379bf9eb80fab32494ebd79ba90653f0
Granth <granthpai1503@gmail.com>
2026-09-08 22:12:56 +0530

    fixed typo

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 
 	cfg := config.MustLoad()
-	_, err :=db.Connect(cfg.DatabaseURL)
+	db, err :=db.Connect(cfg.DatabaseURL)
 	if err != nil {
 		log.Fatalf("main.db.connect: %v", err)
 	}

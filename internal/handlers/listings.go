ae701a20bdf090a54b5d61b77504a3ccc9bb2356
Granth <granthpai1503@gmail.com>
2026-09-09 18:30:48 +0530

    add context

--- internal/handlers/listings.go
+++ internal/handlers/listings.go
@@ -28,7 +28,9 @@ func NewListingHandler(db *sql.DB) *ListingHandler {
 }
 
 func (lh ListingHandler) List (w http.ResponseWriter, r *http.Request) {
-		rows, err := lh.db.Query(
+	ctx := r.Context()
+
+		rows, err := lh.db.QueryContext(ctx,
 			`SELECT id, title, description, price, city, created_at FROM listings
 			  FROM listings
 			  ORDER BY created_at DESC
@@ -65,15 +67,16 @@ func (lh ListingHandler) List (w http.ResponseWriter, r *http.Request) {
 
 
 func (lh ListingHandler) Delete(w http.ResponseWriter, r *http.Request) { 
-		id := r.PathValue("id")
-
-		_, err := lh.db.Exec(
-			`DELETE FROM listings WHERE id = $1`,id)
-			if err != nil {
-				log.Printf("delete: %v", err)
-				http.Error(w, "internal server error", http.StatusInternalServerError)
-				return
-			}
+	ctx := r.Context()
+	id := r.PathValue("id")
+	
+		_, err := lh.db.ExecContext(ctx,
+			`DELETE FROM listings WHERE id = $1`, id)
+		if err != nil {
+			log.Printf("delete: %v", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
 
 			w.WriteHeader(http.StatusNoContent)
 	}
